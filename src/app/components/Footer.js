600adebf3909ab43e5f6a83c755025c41b7f6758
Serge Moraru <90487457+Sergemoraru@users.noreply.github.com>
2023-10-20 20:49:28 -0600

    Adjusted home page and added image

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -9,24 +9,6 @@ const Footer = () => {
     <footer className="bg-red-600 text-white">
       <div className="container mx-auto px-8 py-6">
         <div className="flex justify-between items-center">
-          <div className="text-lg font-semibold">Golden Dragon</div>
-          <div className="space-x-4">
-            <a href="/home" className="hover:text-red-300">
-              Home
-            </a>
-            <a href="/menu" className="hover:text-red-300">
-              Menu
-            </a>
-            <a href="/about" className="hover:text-red-300">
-              About
-            </a>
-            <a href="/contact" className="hover:text-red-300">
-              Contact
-            </a>
-          </div>
-        </div>
-        <hr className="my-4 border-red-300" />
-        <div className="flex justify-between items-center">
           <div className="text-sm">
             &copy; 2023 Golden Dragon. All rights reserved.
           </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Image from "next/image";
 
 const HomePage = () => {
   return (
@@ -9,14 +10,16 @@ const HomePage = () => {
 
       <main className="flex-grow flex items-center justify-center p-8">
         <div className="max-w-3xl w-full text-center space-y-8">
-          <img
-            src="/path_to_hero_image.jpg"
+          <Image
+            src="/golden-dragon.png"
             alt="Golden Dragon Dining"
             className="rounded-lg shadow-md mb-8 w-full h-80 object-cover"
+            width={800}
+            height={320}
           />
 
           <h1 className="text-4xl font-bold text-red-600">
-            Welcome to Golden Dragon
+            Welcome to <span className="text-orange-500">Golden Dragon</span>
           </h1>
 
           <p className="text-xl text-gray-700">
@@ -27,16 +30,22 @@ const HomePage = () => {
           </p>
 
           <div className="mt-8">
-            <a
-              href="mailto:reservations@goldendragon.com"
-              className="bg-red-500 hover:bg-red-600 text-white py-2 px-8 rounded-lg text-xl font-semibold transition-transform transform hover:scale-105"
-            >
-              Make a Reservation
-            </a>
+            <div>
+              <a
+                href="mailto:reservations@goldendragon.com"
+                className="bg-red-500 hover:bg-red-600 text-white py-2 px-8 rounded-lg text-xl font-semibold transition-transform transform hover:scale-105"
+              >
+                Make a Reservation
+              </a>
+            </div>
+            <div className="mt-8">
+              <a className="bg-red-500 hover:bg-red-600 text-white py-2 px-8 rounded-lg text-xl font-semibold transition-transform transform hover:scale-105">
+                The Menu
+              </a>
+            </div>
           </div>
         </div>
       </main>
-
       <Footer />
     </div>
   );
